6aa007bf1d2a1deda75b67271c2bd932f93dc06e
Donald Wade <rwckdw@gmail.com>
2015-12-18 10:00:42 +1100

    changed sinatra app

--- sinatra_sockets.rb
+++ sinatra_sockets.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 require 'tilt/erb'
-# require 'arduino_firmata'
 require 'gon-sinatra'
 require 'sinatra/reloader' if development?
 
@@ -12,12 +11,12 @@ module MyChat
 
     Tilt.register Tilt::ERBTemplate, 'html.erb'
 
-    enable :reloader
+    # enable :reloader
 
-    set :environment, :development
+    # set :environment, :development
 
-    # set :environment, :production
-    # set :bind, 'shrouded-cliffs-5129.herokuapp.com'
+    set :environment, :production
+    set :bind, 'shrouded-cliffs-5129.herokuapp.com'
 
     get '/' do
       erb :index
